98e6225ec041e6299b048a7fea613cf7eaed6c59
Dani <danielle@thirdrailcreative.com>
2023-01-24 15:33:34 -0600

    updated Header

--- src/Views/Header/Header.jsx
+++ src/Views/Header/Header.jsx
@@ -5,12 +5,7 @@ import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { RoomEnvironment } from "three/examples/jsm/environments/RoomEnvironment.js";
-import { ShaderMaterial } from 'three';
-import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
-import { MeshStandardMaterial } from "three";
 import { MeshPhysicalMaterial } from "three";
-
-import { ClearCoatMaterial } from "three";
 import model from "./3DModel/ringtone/model.gltf";
 import "./Header.scss";
 
@@ -19,8 +14,6 @@ gsap.registerPlugin(ScrollTrigger);
 export default function Header(props) {
   const headerContent = useRef(null);
   const containerRef = useRef(null);
-
-  // const modelUrl = `./3DModel/ringtone/scene.gltf?v=${Date.now()}`
 
   useEffect(() => {
     gsap.to(headerContent.current, {
@@ -48,7 +41,7 @@ export default function Header(props) {
     containerRef.current.appendChild(renderer.domElement);
     const pmremGenerator = new THREE.PMREMGenerator(renderer);
     pmremGenerator.compileEquirectangularShader();
-    const envMap = pmremGenerator.fromScene(new RoomEnvironment(), 0.04).texture;
+    const envMap = pmremGenerator.fromScene(new RoomEnvironment()).texture;
     scene.environment = envMap;
     pmremGenerator.dispose();
 
@@ -61,116 +54,90 @@ export default function Header(props) {
     controls.enablePan = false;
     controls.autoRotate = true;
     controls.autoRotateSpeed = 2;
+    controls.update();
 
     const loader = new GLTFLoader();
-    // const dracoLoader = new DRACOLoader();
-    // dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.4.1/');
-    // loader.setDRACOLoader(dracoLoader);
-
     loader.load(model, (gltf) => {
-      console.log('gltf', gltf.material);
 
-      // const materials = gltf.parser.json.materials[0];
-      // for (let i = 0; i < materials.length; i++) {
-      //   const material = materials[i];
-      //   console.log('material', material);
-      // }
-
-
-      const iridescence = gltf.parser.json.materials[0].extensions.KHR_materials_iridescence;
-      console.log('iridescence', iridescence);
-
-      if (iridescence) {
-        const iridescenceMap = new THREE.ShaderMaterial({
-          uniforms: {
-            iridescenceFactor: { value: iridescence.iridescenceFactor },
-            iridescenceIor: { value: iridescence.iridescenceIor },
-            iridescenceThicknessMinimum: { value: iridescence.iridescenceThicknessMinimum },
-            iridescenceThicknessMaximum: { value: iridescence.iridescenceThicknessMaximum },
-          }
-        });
-      
-        // console.log('iridescenceMap', iridescenceMap);
-
-        const clearCoat = gltf.parser.json.materials[0].extensions.KHR_materials_clearcoat;
-        console.log('clearCoat', clearCoat);
-
-        const clearCoatMap = new THREE.ShaderMaterial({
-          uniforms: {
-            clearcoatFactor: { value: clearCoat.clearcoatFactor },
-            clearcoatRoughnessFactor: { value: clearCoat.clearcoatRoughnessFactor },
-          }
-        });
-
-        const pbrMetallicRoughness = gltf.parser.json.materials[0].pbrMetallicRoughness;
-        console.log('pbrMetallicRoughness', pbrMetallicRoughness);
-
-        const pbrMetallicRoughnessMap = new THREE.ShaderMaterial({
-          uniforms: {
-            baseColorFactor: { value: pbrMetallicRoughness.baseColorFactor },
-            metallicFactor: { value: pbrMetallicRoughness.metallicFactor },
-            roughnessFactor: { value: pbrMetallicRoughness.roughnessFactor },
-          }
-        });
-
+      // Shaders and materials
+      const iridescenceShader = gltf.parser.json.materials[0].extensions.KHR_materials_iridescence;
+      const clearCoatShader = gltf.parser.json.materials[0].extensions.KHR_materials_clearcoat;
+      const emissiveShader = gltf.parser.json.materials[0].emissiveFactor;
+      const pbrMetallicRoughness = gltf.parser.json.materials[0].pbrMetallicRoughness;
 
       gltf.scene.traverse((child) => {
-        gltf.scene.scale.set(3, 3, 3);
+        gltf.scene.scale.set(2.5, 2.5, 2.5);
+        gltf.material = new MeshPhysicalMaterial({
+          color: new THREE.Color(pbrMetallicRoughness.baseColorFactor),
+          metalness: pbrMetallicRoughness.metallicFactor,
+          roughness: pbrMetallicRoughness.roughnessFactor,
+          emissive: emissiveShader,
+          _clearcoat: {
+            clearcoat: clearCoatShader.clearcoatFactor,
+            clearcoatRoughness: clearCoatShader.clearcoatRoughnessFactor,
+          },
+          _iridescence: {
+            iridescence: iridescenceShader.iridescenceFactor,
+            iridescenceIOR: iridescenceShader.iridescenceIor,
+            thickness: iridescenceShader.iridescenceThicknessMaximum,
+          },
+          sheenColor: pbrMetallicRoughness.baseColorFactor,
+          _sheen: {
+            color: pbrMetallicRoughness.baseColorFactor,
+            roughness: 0,
+          },
+          reflectivity: 3,
+          attenuationColor: new THREE.Color(0xffffff),
+          attenuationDistance: 0.5,
+          clipShadows: true,
+          envMap: envMap,
+          envMapIntensity: 1,
+        });
+
+        //scroll rotation
+        const initialRotation = Math.PI / 1;
+        gltf.scene.rotation.x = initialRotation;
+        let lastScrollTop = 0;
+        let currentPosition = initialRotation;
+
+        window.addEventListener("scroll", () => {
+          const scrollTop = window.scrollY || document.documentElement.scrollTop;
+          if (scrollTop > lastScrollTop) {
+            currentPosition -= 0.035;
+            gltf.scene.rotation.x = currentPosition;
+          } else {
+            currentPosition += 0.035;
+            if (currentPosition < initialRotation) {
+              currentPosition = initialRotation;
+            }
+            gltf.scene.rotation.x = currentPosition;
+          }
+          if (scrollTop === 0) {
+            gltf.scene.rotation.x = initialRotation;
+            lastScrollTop = scrollTop;
+          }
+        });
+
         if (child.isMesh) {
           child.castShadow = true;
-          child.receiveShadow = true;
-          child.material = iridescenceMap && clearCoatMap && pbrMetallicRoughnessMap ? new THREE.MeshStandardMaterial({
-            color: 0x000000,
-            metalness: 1,
-            roughness: 0.1,
-            envMap: envMap,
-            envMapIntensity: 1,
-            
-            clearcoat: clearCoatMap.uniforms.clearcoatFactor.value,
-            clearcoatRoughness: clearCoatMap.uniforms.clearcoatRoughnessFactor.value,
-
-            iridescence: iridescenceMap.uniforms.iridescenceFactor.value,
-            iridescenceIOR: iridescenceMap.uniforms.iridescenceIor.value,
-            iridescenceThicknessMin: iridescenceMap.uniforms.iridescenceThicknessMinimum.value,
-            iridescenceThicknessMax: iridescenceMap.uniforms.iridescenceThicknessMaximum.value,
-          }) : new THREE.MeshStandardMaterial({
-            color: 0x000000,
-            metalness: 1,
-            roughness: 0.1,
-            envMap: envMap,
-            envMapIntensity: 1,
-          });
-
-          child.layers.enable(1);
-          child.layers.enable(2);
+          child.receiveShadow = false;
+          child.layers.enableAll();
           child.animations = gltf.animations;
           child.mixer = new THREE.AnimationMixer(child);
           child.mixer.clipAction(child.animations[0]).play();
         }
-
       });
-      console.log(gltf.scene);
-      console.log(gltf.materials);
-
-
       scene.add(gltf.scene);
-        console.log(gltf);
-      }
     });
-
-    console.log('log',scene);
 
     const render = () => {
       requestAnimationFrame(render);
       renderer.render(scene, camera);
-
       scene.traverse((child) => {
         if (child.isMesh && child.mixer) {
           child.mixer.update(0.01);
         }
-      }
-      );
-  
+      });
     };
     render();
   }, []);
@@ -180,10 +147,7 @@ export default function Header(props) {
       <div className="header-content" ref={headerContent}>
         <span className="header-span"></span>
         <h1 className="header-title">DEVELOPER</h1>
-        <p className="header-subtitle">
-          Building websites with a passion for clean design, user experience,
-          and modern technologies
-        </p>
+        <p className="header-subtitle">Building websites with a passion for clean design, user experience, and modern technologies</p>
       </div>
     </div>
   );
